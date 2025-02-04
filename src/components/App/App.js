e674e36b53a8cb9056aec1c2a1734388e1b58aa1
EliRaif <e0583269399@gmail.com>
2025-02-04 20:36:51 +0200

    Test commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
 
       <header className="App-header">
-
+        <img src={logo} className="App-logo" alt="logo" />
 
         {/* <Button  text="Dont Me" className="btn-light"/> */}
 
